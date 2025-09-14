53b7e3c29482fe20fcd1e39d82357fe2194c1afa
Damitha Theekshana <theekshanadamitha@gmail.com>
2025-09-14 21:23:13 +0530

    Complete ordercontroller

--- src/main/java/controller/ordercontroller/OrderManagementService.java
+++ src/main/java/controller/ordercontroller/OrderManagementService.java
@@ -1,4 +1,16 @@
 package controller.ordercontroller;
 
+import javafx.collections.ObservableList;
+import model.Order;
+
 public interface OrderManagementService {
+
+    ObservableList<Order> getAllOrders();
+
+    void addOrders(String orderID, String orderDate, String custID);
+
+    void deleteOrder(String orderID);
+
+    void updateOrder(String orderID, String orderDate, String custID);
 }
+
